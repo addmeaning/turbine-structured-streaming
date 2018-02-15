3485487d8e852c1672fa3245abacdcea892a730c
Anton Lastauka <addmeaning@gmail.com>
2018-02-15 09:48:16 +0100

    init

--- src/main/scala/SparkApp.scala
+++ src/main/scala/SparkApp.scala
@@ -1,5 +1,21 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.linalg.{Vector => SparkVector}
+import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.{Dataset, SparkSession}
 
-object SparkApp extends App{
- val spark = SparkSession.builder().appName("spark-app").master("local[*]").getOrCreate()
+object SparkApp extends App {
+  val spark = SparkSession.builder().appName("spark-app").master("local[*]").getOrCreate()
+  spark.sparkContext.setLogLevel("WARN")
+  import spark.implicits._
+
+  val df = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "turbine")
+    .load()
+  private val transformed: Dataset[String] = df.selectExpr("CAST(value AS STRING)")
+    .as[String]
+
+  private val query: StreamingQuery = transformed.writeStream.format("console").start()
+  query.awaitTermination()
 }
